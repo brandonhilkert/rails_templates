d6ef49e58b7548e15555bbbdaf94a66710198acf
Brandon Hilkert <brandonhilkert@gmail.com>
2013-11-16 11:10:20 -0500

    Update using inject_to_file

--- default.rb
+++ default.rb
@@ -16,7 +16,7 @@ environment 'config.action_dispatch.x_sendfile_header = "X-Accel-Redirect"', env
 environment 'config.static_cache_control = "public, max-age=31449600" # 1 year', env: "production"
 
 # Setup configuration
-application_settings = <<-TEMPLATE
+create_file "config/application.yml" do <<-FILE
 defaults: &defaults
 
 development:
@@ -24,19 +24,20 @@ development:
 
 test:
   <<: *defaults
-TEMPLATE
-create_file "config/application.yml", application_settings
+FILE
+end
 
-application_setup = <<-TEMPLATE
-if File.exists?(File.expand_path(\'../application.yml\', __FILE__))
-  config = YAML.load(File.read(File.expand_path(\'../application.yml\', __FILE__)))
+inject_into_file "config/application.rb", before: "module" do <<-FILE
+if File.exists?(File.expand_path('../application.yml', __FILE__))
+  config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
   config.merge! config.fetch(Rails.env, {})
   config.each do |key, value|
     ENV[key] ||= value.to_s unless value.kind_of? Hash
   end
 end
-TEMPLATE
-run "echo #{application_setup} >> config/application.rb"
+
+FILE
+end
 
 gem_group :development, :test do
   gem "rspec-rails"
@@ -71,7 +72,7 @@ run "bundle install"
 run "bundle binstubs rspec-core"
 
 # Setup unicorn for Heroku
-unicorn_config = <<-TEMPLATE
+create_file "config/unicorn.rb" do <<-FILE
 worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
@@ -94,28 +95,28 @@ after_fork do |server, worker|
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
 end
-TEMPLATE
-create_file "config/unicorn.rb", unicorn_config
+FILE
+end
 
-procfile = <<-TEMPLATE
+create_file "Procfile" do <<-FILE
 web: bundle exec unicorn -p $PORT -c ./config/unicorn.rb
-TEMPLATE
-create_file "Procfile", procfile
+FILE
+end
 
 # Setup rspec
 generate "rspec:install"
 
-capybara = <<-TEMPLATE
+create_file "spec/support/capybara.rb" do <<-FILE
 require 'capybara/rails'
 require 'capybara/rspec'
 
 RSpec.configure do |config|
   Capybara.javascript_driver = :selenium
 end
-TEMPLATE
-create_file "spec/support/capybara.rb", capybara
+FILE
+end
 
-database_cleaner = <<-TEMPLATE
+create_file "spec/support/database_cleaner.rb" do <<-FILE
 RSpec.configure do |config|
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
@@ -126,61 +127,138 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 end
-TEMPLATE
-create_file "spec/support/database_cleaner.rb", database_cleaner
+FILE
+end
 
-factory_girl = <<-TEMPLATE
+create_file "spec/support/factory_girl.rb" do <<-FILE
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 end
-TEMPLATE
-create_file "spec/support/factory_girl.rb", factory_girl
+FILE
+end
 
 create_file "spec/support/helpers.rb", ""
 
-time_helper = <<-TEMPLATE
+create_file "app/helpers/time_helper.rb" do <<-FILE
 module TimeHelper
   def timeago(time, options = {})
     options[:class] ||= "timeago"
     content_tag(:time, time.to_s, options.merge(datetime: time.getutc.iso8601)) if time
   end
 end
-TEMPLATE
-create_file "app/helpers/time_helper.rb", time_helper
+FILE
+end
 
 get "http://timeago.yarp.com/jquery.timeago.js", "vendor/assets/javascripts/jquery.timeago.js"
 
 # Assets setup
-run "echo '//= require jquery.timeago' >> app/assets/javascripts/application.js"
+append_file "app/assets/javascripts/application.js", "//= require jquery.timeago"
 
-init = <<-TEMPLATE
+create_file "app/assets/javascripts/init.js.coffee" do <<-FILE
 window.App ||= {}
-TEMPLATE
-create_file "app/assets/javascripts/init.js.coffee", init
-run "echo '//= require init' >> app/assets/javascripts/application.js"
+FILE
+end
+append_file "app/assets/javascripts/application.js", "//= require init"
 
-app_timeago = <<-TEMPLATE
+create_file "app/assets/javascripts/app.timeago.js.coffee" do <<-FILE
 App.TimeAgo =
   replaceTimes: ->
     $("time.timeago").timeago()
-TEMPLATE
-create_file "app/assets/javascripts/app.timeago.js.coffee", app_timeago
-run "echo '//= require app.timeago' >> app/assets/javascripts/application.js"
+FILE
+end
+append_file "app/assets/javascripts/application.js", "//= require app.timeago"
 
-application_css =<<-TEMPLATE
+create_file "app/assets/stylesheets/application.css.scss" do <<-FILE
 /*
  *= require_self
  */
 
 @import "twitter/bootstrap";
 @import "font-awesome";
-TEMPLATE
+FILE
+end
 
-run "rm app/assets/stylesheets/application.css"
-create_file "app/assets/stylesheets/application.css.scss", application_css
+remove_file "app/assets/stylesheets/application.css"
+
+# Optional things
+#
+# Email
+if yes?("Email?")
+  create_file "config/initializers/setup_email.rb" do <<-FILE
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV["SENDGRID_USERNMAE"],
+  :password => ENV["SENDGRID_PASSWORD"],
+  :domain => ENV["EMAIL_DOMAIN"],
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
+ActionMailer::Base.default_url_options = {
+  host: ENV["EMAIL_DOMAIN"]
+}
+
+if Rails.env.development?
+  class OverrideMailRecipient
+    def self.delivering_email(mail)
+      mail.to = ENV["EMAIL_OVERRIDE"]
+    end
+  end
+  ActionMailer::Base.register_interceptor(OverrideMailRecipient)
+end
+FILE
+  end
+
+  inject_into_file "config/application.yml", before: "development" do <<-FILE
+  SENDGRID_USERNAME:
+  SENDGRID_PASSWORD:
+  EMAIL_DOMAIN:
+
+  EMAIL_OVERRIDE:
+
+FILE
+  end
+
+end
+
+# Sidekiq
+if yes?("Sidekiq?")
+  gem 'sidekiq', require: "sidekiq/web"
+  gem 'sinatra'
+
+  create_file "config/initializers/sidekiq.rb" do <<-FILE
+if Rails.env.production?
+  Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+    user == ENV["SIDEKIQ_USERNAME"] && password == ENV["SIDEKIQ_PASSWORD"]
+  end
+end
+FILE
+  end
+
+  create_file "app/workers/sample_worker.rb" do <<-FILE
+class SampleWorker
+  include Sidekiq::Worker
+
+  def perform
+
+  end
+end
+FILE
+  end
+
+  inject_into_file "config/application.yml", before: "development" do <<-FILE
+  SIDEKIQ_USERNAME:
+  SIDEKIQ_PASSWORD:
+
+FILE
+  end
+
+  run "bundle install"
+end
 
 git :init
-run "echo 'config/application.yml' >> .gitignore"
+append_file ".gitignore", "config/application.yml"
 
 git add: "."
 git commit: "-a -m 'Initial commit'"
