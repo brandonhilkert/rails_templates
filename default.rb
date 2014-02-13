c3a5eface1db25f0bc5a6b61336d02cd7ce0e6c1
Brandon Hilkert <brandonhilkert@gmail.com>
2014-02-13 10:27:29 -0500

    Prefer avdi's rspec setup for capybara

--- default.rb
+++ default.rb
@@ -95,16 +95,20 @@ FILE
 end
 
 create_file "spec/support/database_cleaner.rb" do <<-FILE
-
 RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
   config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
 
-    if example.metadata[:js] || example.metadata[:truncation]
-      DatabaseCleaner.strategy = :truncation
-    else
-      DatabaseCleaner.strategy = :transaction
-    end
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
 
+  config.before(:each) do
     DatabaseCleaner.start
   end
 
